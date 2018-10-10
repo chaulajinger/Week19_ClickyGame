30475339f0f183a86eebc4a433a8f88bd1309e5d
chaula Jinger <jingerc4@gmail.com>
2018-10-09 21:00:37 -0400

    Completed

---
+++ clicklygame/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+// sets up the reusable Footer Component
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+	<footer className='footer'>
+		<p>©10/2018 Cjinger</p>
+	</footer>
+
+);
+
+export default Footer;
